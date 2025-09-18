71152c3acc617844cc6a70ceb4c4e5dde004601a
Maxime Banga kollo <bangakollomaxime@gmail.com>
2025-09-18 09:00:30 +0100

    Initial commit - Project generated and database configured

---
+++ src/main/java/com/universite/UniClubs/UniClubsApplication.java
@@ -0,0 +1,13 @@
+package com.universite.UniClubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniClubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniClubsApplication.class, args);
+	}
+
+}
