a770bf936430ffe05e9cd48d8fdb923a9d897bd6
Maxime Banga kollo <bangakollomaxime@gmail.com>
2025-09-24 11:39:26 +0100

    fix(/decouvrir): Corrige l'affichage de la page Decouvrir

--- src/main/java/com/universite/UniClubs/controllers/DiscoveryController.java
+++ src/main/java/com/universite/UniClubs/controllers/DiscoveryController.java
@@ -1,16 +1,30 @@
 package com.universite.UniClubs.controllers;
 
 
+import com.universite.UniClubs.entities.Utilisateur;
+import com.universite.UniClubs.repositories.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/decouvrir")
 public class DiscoveryController {
 
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
+
     @GetMapping
-    public String showDiscoveryPortal(){
+    public String showDiscoveryPortal(Model model, Principal principal ) {
+
+        String email = principal.getName();
+        Utilisateur utilisateurConnecte = utilisateurRepository.findByEmail(email).orElse(null);
+        model.addAttribute("utilisateur", utilisateurConnecte);
+
         return "decouvrir";
     }
 }
