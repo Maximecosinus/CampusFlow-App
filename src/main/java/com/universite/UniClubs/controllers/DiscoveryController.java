dd457535e10734969c4bb6dd8035d5de19217835
Maxime Banga kollo <bangakollomaxime@gmail.com>
2025-09-24 12:24:06 +0100

    feat(clubs-list.html): mise en place du bouton 'explorer les clubs' et de la page où on voit tous les clubs

--- src/main/java/com/universite/UniClubs/controllers/DiscoveryController.java
+++ src/main/java/com/universite/UniClubs/controllers/DiscoveryController.java
@@ -3,6 +3,7 @@ package com.universite.UniClubs.controllers;
 
 import com.universite.UniClubs.entities.Utilisateur;
 import com.universite.UniClubs.repositories.UtilisateurRepository;
+import com.universite.UniClubs.services.ClubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,9 @@ public class DiscoveryController {
     @Autowired
     private UtilisateurRepository utilisateurRepository;
 
+    @Autowired
+    private ClubService clubService;
+
     @GetMapping
     public String showDiscoveryPortal(Model model, Principal principal ) {
 
@@ -26,5 +30,15 @@ public class DiscoveryController {
         model.addAttribute("utilisateur", utilisateurConnecte);
 
         return "decouvrir";
+    }
+
+    @GetMapping("/clubs")
+    public String showClubsListPage(Model model, Principal principal ) {
+        String email = principal.getName();
+        Utilisateur utilisateurConnecte = utilisateurRepository.findByEmail(email).orElse(null);
+        model.addAttribute("utilisateur", utilisateurConnecte);
+        model.addAttribute("allClubs",clubService.getAllClubs());
+
+    return "clubs-list";
     }
 }
