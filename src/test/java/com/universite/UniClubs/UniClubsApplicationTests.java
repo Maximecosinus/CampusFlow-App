e047deaf4910a1f320d5a3c6d06c7d5a5d7d9eda
Maxime Banga kollo <bangakollomaxime@gmail.com>
2025-10-10 12:18:35 +0100

    fix: Configuration des tests avec H2 pour CI/CD

--- src/test/java/com/universite/UniClubs/UniClubsApplicationTests.java
+++ src/test/java/com/universite/UniClubs/UniClubsApplicationTests.java
@@ -2,8 +2,10 @@ package com.universite.UniClubs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UniClubsApplicationTests {
 
 	@Test
